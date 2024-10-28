cacf34d76a6b48c7df55cb28fdf4918a91064dd4
bachgialight <89329211+bachgialight@users.noreply.github.com>
2024-10-28 08:54:51 +0700

    cap nhat code1

---
+++ src1/config/view_engines.js
@@ -0,0 +1,13 @@
+
+const path = require('path');
+const express = require('express');
+
+const configViewEngine = (app) => {
+
+    app.set("views", "./src/views");
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, 'public')));
+
+}
+
+module.exports = configViewEngine;
